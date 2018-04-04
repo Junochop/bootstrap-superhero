7096562fe51478062bd475851de10f001d38ee6c
Juno Lee <lejuno80@gmail.com>
2018-04-03 21:20:34 -0500

    added card

--- javascripts/jobs.js
+++ javascripts/jobs.js
@@ -1,44 +1,84 @@
-const printToDom = (domString, divID) => {
-    document.getElementById(divID).innerHTML += domString;
+let selectedHero = "";
 
+const printToDom = (domString, divId) => {
+    document.getElementById(divId).innerHTML = domString;
 };
 
-const builddomString = (superHeroArray) => {
+const buildDomString = heroes => {
     let domString = "";
-    superHeroArray.forEach((hero) => {
-
+    for (let i = 0; i < heroes.length; i++) {
         domString += `<li>`;
-        domString += `<a href="#" data-hero-id="${hero.id}">${hero.name}</a>`;
-        domString += `</li>`;
-        
-
-
-
-    });
-
+        domString += `<a class="hero-name" data-hero-id="${heroes[i].id}">${
+            heroes[i].name
+            }</a>`;
+        domString += ` </li>`;
+    }
     printToDom(domString, "awesome-dropdown");
 };
 
+const selectHero = e => {
+    selectedHero = e.target.dataset.heroId;
+    document.getElementById('awesome-button').classList.add('hide');
+    genericHeroRequest(loadFileforSingleHero);
+};
 
-function executeThisFunctionAfterFileLoads() {
+const addheroSelectionEventListeners = () => {
+    const heroNames = document.getElementsByClassName("hero-name");
+    for (let i = 0; i < heroNames.length; i++) {
+        heroNames[i].addEventListener("click", selectHero);
+    }
+};
 
+const displaySuperhero = heroes => {
+    let domString = "";
+    heroes.forEach(hero => {
+        if (hero.id === selectedHero) {
+            domString += `<div class="row">`;
+            domString += `<div class="col-sm-4">`;
+            if (hero.gender === "Male") {
+                domString += `<img class="charImage maleImage" src="${
+                    hero.image
+                    }">`;
+            } else {
+                domString += `<img class="charImage femaleImage" src="${
+                    hero.image
+                    }">`;
+            }
+            domString += `</div>`;
+            domString += `<div class="col-sm-6">`;
+            domString += `<h2>Selected Hero: ${hero.name}</h2>`;
+            domString += `<p class='charDescription'>${hero.description}</p>`;
+            domString += `</div>`;
+        }
+    });
+    printToDom(domString, "selected-hero");
+};
+
+function loadFileforSingleHero() {
     const data = JSON.parse(this.responseText);
-    console.log("data", data);
-    builddomString(data.superheroes);
-
+    displaySuperhero(data.superheroes);
 }
 
-function WTF() {
-    console.log("something went wrong");
+function executeThisCodeIfXHRFails() {
+    console.log("something broke");
 }
 
-const startApplication = () => {
+function executeThisCodeAfterFileLoaded() {
+    const data = JSON.parse(this.responseText);
+    buildDomString(data.superheroes);
+    addheroSelectionEventListeners();
+}
+
+const genericHeroRequest = successFunction => {
     let myRequest = new XMLHttpRequest();
-    myRequest.addEventListener("load", executeThisFunctionAfterFileLoads);
-    myRequest.addEventListener("error", WTF);
+    myRequest.addEventListener("load", successFunction);
+    myRequest.addEventListener("error", executeThisCodeIfXHRFails);
     myRequest.open("GET", "../db/superheroes.json");
     myRequest.send();
-    console.log("myrequest", myRequest);
+};
+
+const startApplication = () => {
+    genericHeroRequest(executeThisCodeAfterFileLoaded);
 };
 
 startApplication();
